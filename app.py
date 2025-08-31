7eba4e6229783e651ea1a771bee308bb30793449
Manpreet Singh Kashyap <singhmanpreet102005@gmail.com>
2025-08-31 18:44:58 +0530

    fixed some issues and made it modular

--- app.py
+++ app.py
@@ -1,34 +1,10 @@
 import numpy as np
 import pandas as pd
 import joblib
-from jinja2 import Environment, Template
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request , jsonify
+from util import processing_inputs
 
 
-skin_thickness_df = pd.read_parquet("skinthickness.parquet")
-scaler = joblib.load("scaler.pkl")
-model = joblib.load("Diabetes-Prediction.pkl")
-
-def bmi_calc(weight, height):
-    return weight / (height ** 2)
-
-def diabetes_pedigree(first_degree_diabetes, second_degree_diabetes, age):
-    base = 0.1
-    if first_degree_diabetes == 'yes' and second_degree_diabetes == 'yes':
-        base = 0.7 + 0.4
-    elif first_degree_diabetes == 'yes':
-        base = 0.7
-    elif second_degree_diabetes == 'yes':
-        base = 0.4
-    
-    if age is not None:
-        if age < 40:
-            base *= 1.2
-        elif age < 60:
-            base *= 1.1
-    return min(base, 1)
-
-# Flask App
 app = Flask(__name__)
 
 @app.route("/", methods=['GET'])
@@ -50,48 +26,39 @@ def predict():
             f_d_y = request.form.get('first_degree_diabetes')
             s_d_y = request.form.get('second_degree_diabetes')
 
-            if not all([age, pregnancies, glucose, bp, insulin, weight, height, f_d_y, s_d_y]):
-                error_message = "Please fill all fields!"
-                return render_template("index.html", prediction=error_message)
-
-            age = int(age)
-            pregnancies = int(pregnancies)
-            glucose = float(glucose)
-            bp = float(bp)
-            insulin = float(insulin)
-            weight = float(weight)
-            height = float(height)
-
-            bmi = bmi_calc(weight, height)
-
-            dpf = diabetes_pedigree(f_d_y, s_d_y, age)
-
-            skin_thickness = float(np.mean(skin_thickness_df))
-            X_cols = {
-                'Pregnancies': [pregnancies],
-                'Glucose': [glucose],
-                'BloodPressure': [bp],
-                'SkinThickness': [skin_thickness],
-                'Insulin': [insulin],
-                'BMI': [bmi],
-                'DiabetesPedigreeFunction': [dpf],
-                'Age': [age]
-            }
-            X = pd.DataFrame(X_cols)
-
-            scaled_X = scaler.transform(X)
-            prediction_bin= model.predict(scaled_X)[0]
+            prediction_bin = processing_inputs(age, pregnancies, glucose, bp, insulin, weight, height, f_d_y, s_d_y)            
             if prediction_bin == 1:
                 prediction = "Diabetic"
             elif prediction_bin == 0:
                 prediction = "Non Diabetic"
             else:
-                prediction = None
+                prediction=prediction_bin
         except Exception as e:
-            error_message = f"Error: {str(e)}"
+            error_message = "Enter Valid Inputs"
             return render_template("index.html", prediction=error_message)
 
-    return render_template("index.html", prediction=prediction)
+    return render_template("index.html",prediction=prediction)
+
+app.route("/api/predict",methods=['POST'])
+def api_predict():
+    prediction = None
+    if request.method == 'POST':
+        try:
+             data=request.get_json(force=True)
+             age = data.get('age')
+             pregnancies = data.get('pregnancies')
+             glucose = data.get('glucose_level')
+             bp = data.get('bp_level')
+             insulin = data.get('insulin_level')
+             weight = data.get('weight')
+             height = data.get('height')
+             f_d_y = data.get('first_degree_diabetes')
+             s_d_y = data.get('second_degree_diabetes')
+             prediction=processing_inputs(age,pregnancies,glucose,bp,insulin,weight,height,f_d_y,s_d_y)
+        except Exception as e:
+            error_message=f'Error: {str(e)}'
+            return jsonify(error_message)
+    return jsonify(prediction)
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ util.py
@@ -0,0 +1,59 @@
+import numpy as np
+import pandas as pd
+import joblib
+def bmi_calc(weight, height):
+    return weight / (height ** 2)
+
+def diabetes_pedigree(first_degree_diabetes, second_degree_diabetes, age):
+    base = 0.1
+    if first_degree_diabetes == 'yes' and second_degree_diabetes == 'yes':
+        base = 0.7 + 0.4
+    elif first_degree_diabetes == 'yes':
+        base = 0.7
+    elif second_degree_diabetes == 'yes':
+        base = 0.4
+    
+    if age is not None:
+        if age < 40:
+            base *= 1.2
+        elif age < 60:
+            base *= 1.1
+    return base
+
+skin_thickness_df = pd.read_parquet("skinthickness.parquet")
+scaler = joblib.load("scaler.pkl")
+model = joblib.load("Diabetes-Prediction.pkl")
+
+def processing_inputs(age,pregnancies,glucose,bp,insulin,weight,height,f_d_y,s_d_y):
+    age = int(age)
+    pregnancies = int(pregnancies)
+    glucose = float(glucose)
+    bp = float(bp)
+    insulin = float(insulin)
+    weight = float(weight)
+    height = float(height)
+    bmi = bmi_calc(weight, height)
+    dpf = diabetes_pedigree(f_d_y, s_d_y, age)
+    skin_thickness = float(np.mean(skin_thickness_df))
+
+    x=[age,glucose,bp,insulin,bmi]
+    for value in x:
+        if x<=0:
+            error_message="Error: Enter Valid Inputs"
+            return error_message           
+
+    X_cols = {
+        'Pregnancies': [pregnancies],
+        'Glucose': [glucose],
+        'BloodPressure': [bp],
+        'SkinThickness': [skin_thickness],
+        'Insulin': [insulin],
+        'BMI': [bmi],
+        'DiabetesPedigreeFunction': [dpf],
+        'Age': [age]
+    }
+    
+    X = pd.DataFrame(X_cols)
+    scaled_X = scaler.transform(X)
+    prediction= model.predict(scaled_X)[0]
+    return prediction,error_message
